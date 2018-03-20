06c3a88efcf25d299c5fdc636fb92988ecc7d932
Bogdan Kulynych <hello@bogdankulynych.me>
2018-03-20 20:00:47 +0100

    Fix test running; add Travis

--- setup.py
+++ setup.py
@@ -3,24 +3,17 @@
 from setuptools import setup
 
 install_requires = [
-    'six==1.11.0',
-    'msgpack==0.5.6'
-]
-tests_require = install_requires + [
-    "pytest==3.4.2",
+    'six>=1.11.0',
+    'msgpack>=0.5.6'
 ]
 
 setup(
     name='hippiepug',
-    version=0.0,
+    version=0.1,
     description='Merkle trees and hash chains with a flexible storage backend.',
     author='Bogdan Kulynych',
     author_email='hello@bogdankulynych.me',
     packages=['hippiepug'],
     license="MIT",
-    install_requires=install_requires,
-    tests_require=tests_require,
-    extras={
-        'test': tests_require
-    }
+    install_requires=install_requires
 )
