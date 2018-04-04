102eda9af6eca445299e61f452b6050e46f81002
Bogdan Kulynych <hello@bogdankulynych.me>
2018-04-04 15:28:12 +0200

    Bump version; update URL

--- hippiepug/__init__.py
+++ hippiepug/__init__.py
@@ -1,8 +1,13 @@
-__version__ = '0.1'
+__version__ = '0.1.1'
 __title__ = 'hippiepug'
 __author__ = 'Bogdan Kulynych'
 __email__ = 'hello@bogdankulynych.me'
 __copyright__ = 'Copyright (c) 2018 Bogdan Kulynych, EPFL SPRING Lab'
 __description__ = 'Sublinear-traversal blockchains and efficient key-value Merkle trees with a flexible storage backend'
-__url__ = 'https://github.com/bogdan-kulynych/hippiepug'
+__url__ = 'https://github.com/spring-epfl/hippiepug'
 __license__ = 'AGPLv3+'
+
+
+from .chain import BlockBuilder, Chain
+from .tree import TreeBuilder, Tree
+from .pack import EncodingParams, encode, decode
